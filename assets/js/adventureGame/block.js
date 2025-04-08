072e466b8e6f219f94951585d097e78d8002a5df
Ruhaan Bansal <ruhamobile@gmail.com>
2025-04-08 14:07:48 -0700

    grok.ai

--- assets/js/adventureGame/block.js
+++ assets/js/adventureGame/block.js
@@ -3,7 +3,7 @@ class Block {
       this.gameEnv = gameEnv;
       this.width = 50; // Width of the block
       this.height = 50; // Height of the block
-      this.x = Math.random() * (gameEnv.innerWidth - this.width); // Random horizontal position
+      this.x = Math.random() * (gameEnv.innerWidth - this.width); // Random starting horizontal position
       this.y = 0; // Start from top of the screen
       this.speed = 2; // Falling speed
       this.color = 'red'; // Color of the block
@@ -13,8 +13,9 @@ class Block {
       this.y += this.speed; // Block falls down
   
       if (this.y > this.gameEnv.innerHeight) { 
-        // If block hits the bottom, reset it to the top
+        // If block hits the bottom, reset it to the top and move the column
         this.y = -this.height;
+        this.x = Math.random() * (this.gameEnv.innerWidth - this.width); // New random horizontal position
       }
     }
   
@@ -26,4 +27,3 @@ class Block {
   }
   
   export default Block;
-  
