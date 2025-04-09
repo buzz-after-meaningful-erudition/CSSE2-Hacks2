46add07becfe0f69710462f6a335712d857eef39
Ruhaan Bansal <ruhamobile@gmail.com>
2025-04-09 13:35:29 -0700

    implent endereye

--- assets/js/adventureGame/GameLevelEnd.js
+++ assets/js/adventureGame/GameLevelEnd.js
@@ -103,18 +103,18 @@ class GameLevelEnd {
     
   
 
-    const sprite_src_tux = path + "/images/gamify/tux.png";
-    const sprite_greet_tux = "THIS IS HOW IT ENDS - Tejo :P";
-    const sprite_data_tux = {
-        id: 'Tux',
-        greeting: sprite_greet_tux,
-        src: sprite_src_tux,
+    const sprite_src_endereye = path + "/images/gamify/endereye.png";
+    const sprite_greet_endereye = "THIS IS HOW IT ENDS - Tejo :P";
+    const sprite_data_endereye = {
+        id: 'Endereye',
+        greeting: sprite_greet_endereye,
+        src: sprite_src_endereye,
         SCALE_FACTOR: 8,
         ANIMATION_RATE: 1000000,
-        pixels: {height: 256, width: 352},
+        pixels: {height: 256, width: 256},
         INIT_POSITION: { x: (width / 2), y: (height / 2) },
-        orientation: {rows: 8, columns: 11 },
-        down: {row: 5, start: 0, columns: 3 },
+        orientation: {rows: 2, columns: 2 },
+        down: {row: 1, start: 0, columns: 1 },
         hitbox: { widthPercentage: 0.1, heightPercentage: 0.2 },
         zIndex: 10,  // Same z-index as player
         quiz: { 
@@ -124,12 +124,12 @@ class GameLevelEnd {
           ] 
         },
         reaction: function() {
-          alert(sprite_greet_tux);
+          alert(sprite_greet_endereye);
         },
         interact: function() {
           let quiz = new Quiz();
           quiz.initialize();
-          quiz.openPanel(sprite_data_tux);
+          quiz.openPanel(sprite_data_endereye);
         }
     };
 
@@ -138,7 +138,7 @@ class GameLevelEnd {
       { class: BackgroundParallax, data: image_data_parallax },  // Add parallax background first
       { class: GamEnvBackground, data: image_data_end },         // Then regular background
       { class: Player, data: sprite_data_chillguy },
-      { class: Npc, data: sprite_data_tux },
+      { class: Npc, data: sprite_data_endereye },
       { class: Player, data: sprite_data_alex }
     ];
     console.log("Classes array created with", this.classes.length, "items");
@@ -154,7 +154,7 @@ class GameLevelEnd {
     img.src = imageSrc;
   }
   
-  /**
+  /*
    * Create a debug button to show/test the base path
    */
   createPathDebugger(path) {
